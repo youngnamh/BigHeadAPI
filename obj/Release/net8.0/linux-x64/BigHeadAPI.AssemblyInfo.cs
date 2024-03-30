7b1fe03b00b6054a41b25aec6dd6d369d409276d
youngnamh <youngnamhlady@gmail.com>
2024-03-30 19:46:38 -0400

    working poststrava

--- obj/Release/net8.0/linux-x64/BigHeadAPI.AssemblyInfo.cs
+++ obj/Release/net8.0/linux-x64/BigHeadAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BigHeadAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08bf5bf6b14fd1cb9c4593f6ad798c1fabdb4062")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+217419eac51f9587fbd3a83280d72dd294a9b315")]
 [assembly: System.Reflection.AssemblyProductAttribute("BigHeadAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BigHeadAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
