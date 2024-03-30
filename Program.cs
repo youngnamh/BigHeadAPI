08bf5bf6b14fd1cb9c4593f6ad798c1fabdb4062
youngnamh <youngnamhlady@gmail.com>
2024-03-30 15:08:37 -0400

    added CORS

--- Program.cs
+++ Program.cs
@@ -6,11 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
 builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 
 builder.Services.AddScoped<StravaAuthService>();
+builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("bhWebPage", policyBuilder =>
+    {
+        policyBuilder.WithOrigins("https://bigheadchallenge.com");
+        policyBuilder.AllowAnyHeader();
+        policyBuilder.AllowAnyMethod();
+        policyBuilder.AllowCredentials();
+    });
+});
 
 var app = builder.Build();
 
@@ -23,11 +33,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
-
 app.MapGet("/", () => "Hello from AWS Lambda");
 
-app.MapGet("/test", () => "Testing testing 3,2,1...");
+
+app.MapControllers();
+
+app.UseCors("bhWebPage");
+
+//app.MapGet("/test", () => "Testing testing 3,2,1...");
 
 
 app.Run();
